f5d630208625d3cc71371305f300b506dd237522
MilanRQ <Milan1504X@gmail.com>
2024-10-25 16:23:38 +0200

    Adding more Stars und Features

--- scripts/starv2.gd
+++ scripts/starv2.gd
@@ -5,7 +5,6 @@ var speed = 5
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate_y(speed * delta)
